ea8e75603c6e8ca82771c21ec84e36fb9eba71e7
JonasBorgesLM <jonasleo92@yahoo.com.br>
2025-04-23 13:53:52 -0300

    refactor: :recycle: refactor SelectionSort tests for consistency

--- algorithms/pkg/selection_sort_test.go
+++ algorithms/pkg/selection_sort_test.go
@@ -12,27 +12,49 @@ var _ = ginkgo.Describe("SelectionSort", func() {
 	ginkgo.Context("when sorting an array", func() {
 
 		ginkgo.It("should return an empty array when input is empty", func() {
-			gomega.Expect(pkg.SelectionSort([]int{})).To(gomega.Equal([]int{}))
+			arr := []int{}
+
+			sortedArr := pkg.SelectionSort(arr)
+
+			gomega.Expect(sortedArr).To(gomega.Equal([]int{}))
 		})
 
 		ginkgo.It("should return the same array when it contains one element", func() {
-			gomega.Expect(pkg.SelectionSort([]int{5})).To(gomega.Equal([]int{5}))
+			arr := []int{5}
+
+			sortedArr := pkg.SelectionSort(arr)
+
+			gomega.Expect(sortedArr).To(gomega.Equal([]int{5}))
 		})
 
 		ginkgo.It("should return the same array when it is already sorted", func() {
-			gomega.Expect(pkg.SelectionSort([]int{1, 2, 3, 4, 5})).To(gomega.Equal([]int{1, 2, 3, 4, 5}))
+			arr := []int{1, 2, 3, 4, 5}
+
+			sortedArr := pkg.SelectionSort(arr)
+
+			gomega.Expect(sortedArr).To(gomega.Equal([]int{1, 2, 3, 4, 5}))
 		})
 
 		ginkgo.It("should sort an unsorted array", func() {
-			gomega.Expect(pkg.SelectionSort([]int{5, 3, 4, 1, 2})).To(gomega.Equal([]int{1, 2, 3, 4, 5}))
+			arr := []int{5, 3, 4, 1, 2}
+
+			sortedArr := pkg.SelectionSort(arr)
+
+			gomega.Expect(sortedArr).To(gomega.Equal([]int{1, 2, 3, 4, 5}))
 		})
 
 		ginkgo.It("should handle arrays with duplicates", func() {
-			gomega.Expect(pkg.SelectionSort([]int{3, 1, 2, 1, 3})).To(gomega.Equal([]int{1, 1, 2, 3, 3}))
+			arr := []int{3, 1, 2, 1, 3}
+
+			sortedArr := pkg.SelectionSort(arr)
+
+			gomega.Expect(sortedArr).To(gomega.Equal([]int{1, 1, 2, 3, 3}))
 		})
 
 		ginkgo.It("should return an empty array when input is nil", func() {
-			gomega.Expect(pkg.SelectionSort(nil)).To(gomega.Equal([]int{}))
+			sortedArr := pkg.SelectionSort(nil)
+
+			gomega.Expect(sortedArr).To(gomega.Equal([]int{}))
 		})
 	})
 })
